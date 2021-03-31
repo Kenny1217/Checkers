4adc18119cb9a6859b0d2b26f9d06952da34dfc7
Kenny <kmorris1217@gmail.com>
2021-03-30 23:04:23 -0400

    Added BoardModel Constructor

--- Classes/BoardModel.cs
+++ Classes/BoardModel.cs
@@ -21,7 +21,23 @@ namespace Checkers.Classes
                 { -1, 2, -1, 2, -1, 2, -1, 2},
                 { 2, -1, 2, -1, 2, -1, 2, -1},
                 { -1, 2, -1, 2, -1, 2, -1, 2}};
+        private int selectedCol; //Column piece is current at
+        private int selectedRow; //Row piece is current at
+        private int moveCol; //Column piece wants to move to
+        private int moveRow; //Row piece wants to move to
+        private int redPiecesLeft; //Number of red pieces left
+        private int blackPiecesLeft;  //Number of black pieces left
 
+        //Constructor
+        public BoardModel()
+        {
+            selectedCol = 0;
+            selectedRow = 0;
+            moveCol = 0;
+            moveRow = 0;
+            redPiecesLeft = 12;
+            blackPiecesLeft = 12;
+        }
 
 
     }
